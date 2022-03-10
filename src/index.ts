2c56b91c73eb4aedba89b5cd9db50f8f79aa9d67
Jackson McMath <jacksonmcmath@gmail.com>
2022-03-10 09:22:57 -0500

    some graphql boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+import { ApolloServer } from "apollo-server";
+import { schema } from "./schema";
+
+export const server = new ApolloServer({
+  schema,
+});
+
+const port = 3000;
+server.listen({ port }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
